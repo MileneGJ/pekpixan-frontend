1d22081b76654f024576afc92a39da738353dd05
milenegj <milene.jannetti@gmail.com>
2023-06-12 11:50:44 -0600

    created initial app configurations and routes

--- src/App.js
+++ src/App.js
@@ -1,24 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import "./css/generalLayout.css";
+import HomePage from "./components/HomePage";
+import DogsPage from "./components/DogsPage";
+import DogDetails from "./components/DogDetails";
+import DisciplinesPage from "./components/DisciplinesPage";
+import DisciplineDetails from "./components/DisciplineDetails";
+import Header from "./components/Layout/Header";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Header />
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/dogs" element={<DogsPage />} />
+        <Route path="/disciplines" element={<DisciplinesPage />} />
+        <Route
+          path="/disciplines/:disciplineId"
+          element={<DisciplineDetails />}
+        />
+        <Route path="/dogs/:dogId" element={<DogDetails />} />
+        <Route path="/dogs/:dogId/disciplines" element={<DisciplinesPage />} />
+        <Route
+          path="/dogs/:dogId/disciplines/:disciplineId"
+          element={<DisciplineDetails />}
+        />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
